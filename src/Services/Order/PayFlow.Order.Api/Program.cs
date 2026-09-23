067956a21c5accc7570dcefdd8c2c717e6ba368b
Serhiy <120639693+Sergiy2k1@users.noreply.github.com>
2026-09-23 20:44:27 +0300

    chore: wire Order service dependencies

--- src/Services/Order/PayFlow.Order.Api/Program.cs
+++ src/Services/Order/PayFlow.Order.Api/Program.cs
@@ -1,4 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using PayFlow.Order.Application.Abstractions;
+using PayFlow.Order.Application.Orders.CreateOrder;
+using PayFlow.Order.Infrastructure.Persistence;
+using PayFlow.Order.Infrastructure.Persistence.Repositories;
+using PayFlow.Order.Infrastructure.Time;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var orderDatabaseConnectionString =
+    builder.Configuration.GetConnectionString("OrderDatabase")
+    ?? throw new InvalidOperationException(
+        "Connection string 'OrderDatabase' is not configured.");
+
+builder.Services.AddDbContext<OrderDbContext>(
+    options => options.UseNpgsql(orderDatabaseConnectionString));
+
+builder.Services.AddScoped<IOrderRepository, OrderRepository>();
+builder.Services.AddScoped<IUnitOfWork, EfUnitOfWork>();
+builder.Services.AddSingleton<IClock, SystemClock>();
+builder.Services.AddScoped<CreateOrderHandler>();
 
 var app = builder.Build();
 
